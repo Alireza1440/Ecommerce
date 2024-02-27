7472ae78fe169f212d5a3d75e5a552541b644444
alireza <alireza1382alir@gmail.com>
2024-02-27 14:33:53 +0330

    Fix some 404

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,7 +4,7 @@ function Product({ id, src, alt, name, price }) {
   return (
     <div>
       <Link to={`product/${id}`}>
-        <img className="w-[200px] h-[200px] bg-gray-200 cursor-pointer rounded-[6px]" src={src} alt={alt} />
+        <img className="w-[200px] h-[200px] bg-gray-200 cursor-pointer rounded-[6px]" src={`.${src}`} alt={alt} />
       </Link>
       <h3 className="my-2">{name}</h3>
       <h4>{price}</h4>
