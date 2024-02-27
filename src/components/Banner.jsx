6bfa3d857046c91f3192efd3bb180704caa12f29
alireza <alireza1382alir@gmail.com>
2024-02-27 17:57:34 +0330

    Fix scroll to products

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,7 +8,9 @@ function Banner() {
         <img src="images/headphones_a_3.webp" alt="Headphone"
           className="shadow-black shadow-2xl absolute h-[70vh] right-[300px] top-0" />
         <button className="mt-4 py-2 px-6 rounded-[4px] bg-red-600 text-white">
-          <a href="#products">خرید</a>
+          <span className="pointer" onClick={() => {
+            document.getElementById('products').scrollIntoView()
+          }} href="/products">خرید</span>
         </button>
       </div>
     </div>
